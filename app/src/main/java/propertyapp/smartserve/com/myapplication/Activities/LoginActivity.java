b230d8bbe18913058062e366f498a67efe17af09
danjohan <bestbuysystems01@gmail.com>
2018-06-18 18:33:56 +0530

    Splash,Login.Signup,maindashboard Completed

---
+++ app/src/main/java/propertyapp/smartserve/com/myapplication/Activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package propertyapp.smartserve.com.myapplication.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import propertyapp.smartserve.com.myapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
